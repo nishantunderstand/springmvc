8ccf6805f98a03839293c8d70359f31a77645d18
Nishant Raj <nishant.read@gmail.com>
2025-10-20 02:23:04 +0530

    LenovoBat 20-Oct-25  2:23:04.27

--- src/main/java/springmvc/controller/BasicController.java
+++ src/main/java/springmvc/controller/BasicController.java
@@ -2,24 +2,36 @@ package springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+// @RequestMapping("/v1") 
+// Best Pratice to Add Versioning
 public class BasicController {
 
-	// Maps root URL /springmvc/ to about.jsp
-	@RequestMapping(value = { "/", "" })
-	public String aboutNew() {
-		return "welcome"; // This returns the view name "about.jsp"
-	}
-		
-	@RequestMapping("/about")
-	public String about() {
-		return "about";
-	}
+  // Maps root URL /springmvc/ to about.jsp
+  @RequestMapping(value = {"/", ""}) // This is Handling Root, And Empty
+  public String aboutNew() {
+    System.out.println("=============Testing123===========");
+    return "welcome"; // This returns the view name "about.jsp"
+  }
 
-	@RequestMapping("/home")
-	public String home() {
-		return "home";
-	}	
+  @RequestMapping("/about")
+  public String about() {
+    return "about";
+  }
+
+  @RequestMapping(value = "/about1",method = RequestMethod.GET)
+  public String about1() {
+    return "about";
+  }
+  
+  @RequestMapping("/home")
+  public String home() {
+    return "home";
+  }
 
 }
+
+
+// @RequestMapping : Class Level , Method Level
